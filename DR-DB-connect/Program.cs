f912b207070a70b7ec5b7737d95dc4c95975fc24
Dazsta <warddarren111@gmail.com>
2021-01-31 16:38:27 +0000

    DW - First initial commit and branch setup

---
+++ DR-DB-connect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DR_DB_connect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
